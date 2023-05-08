d92080774d947f3a127ddce59c626125b4696921
mari-tk <mariia@tkachenko.io>
2023-05-08 14:10:35 -0700

    Add server

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const logger = require('morgan');
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+require('./config/database');
+
+const app = express();
+
+//middleware
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'build')));
+app.use(require('./config/checkToken'));
+
+//routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/chats', require('./routes/api/chats'));
+
+//catch all
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+//listener
+const port = process.env.PORT || 3000;
+server.listen(port, function () {
+  console.log(`Express app running on port ${port}`)
+});
