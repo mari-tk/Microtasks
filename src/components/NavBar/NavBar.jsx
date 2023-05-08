dbd136c97f56ad8d594eb89d8bc4d56bc79ba95e
mari-tk <mariia@tkachenko.io>
2023-05-08 16:55:35 -0700

    Add NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,7 +1,41 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import * as userService from '../../utilities/users-service';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 
-export default function NavBar() {
+export default function NavBar({user, updateUser}) {
+
+  function handleLogOut() {
+    userService.logOut();
+    updateUser(null);
+  }
+
   return (
-    <div>NavBar</div>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar>
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            {/* <MenuIcon /> */}
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Welcome to Microtasks, {user.name}
+          </Typography>
+          <Button onClick={handleLogOut} color="inherit">Logout</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
   )
 }
+
