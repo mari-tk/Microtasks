30db4e02418a3a0acba8988280b1e4d88cf9293f
mari-tk <mariia@tkachenko.io>
2023-05-08 14:09:01 -0700

    Add Signup and User route, controller and api

---
+++ src/utilities/users-api.js
@@ -0,0 +1,12 @@
+// API modules are where the code lives to communicate
+// with the server via AJAX
+import sendRequest from './send-request';
+const BASE_URL = '/api/users';
+
+export function signUp(userData) {
+  return sendRequest(BASE_URL, 'POST', userData);
+}
+
+export function login(credentials) {
+  return sendRequest(`${BASE_URL}/login`, 'POST', credentials);
+}
