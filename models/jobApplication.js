d053291dd25f70f898c6a87c27c56cf62f693aa4
mari-tk <mariia@tkachenko.io>
2023-05-10 21:40:04 -0700

    Add job application schema

---
+++ models/jobApplication.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const jobApplicationSchema = new Schema({
+  letter: { 
+    type: String, 
+    required: true 
+  },
+  userId: { 
+    type: Schema.Types.ObjectId, 
+    ref: 'User',
+    required: true  
+  },
+  jobId: { 
+    type: Schema.Types.ObjectId, 
+    ref: 'Job',
+    required: true  
+  },
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('JobApplication', jobApplicationSchema);
