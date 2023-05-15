d2a89401e9af95aad3e61848ea0c1955e1cb45b5
mari-tk <mariia@tkachenko.io>
2023-05-14 23:54:41 -0700

    Edit styles for Dashboard page

--- src/pages/MyDashboardPage/MyDashboardPage.jsx
+++ src/pages/MyDashboardPage/MyDashboardPage.jsx
@@ -1,5 +1,16 @@
 import React, { useEffect, useState } from 'react';
-import { Container } from '@mui/material';
+import {
+  Container,
+  Grid,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow,
+  Toolbar,
+  Typography,
+} from '@mui/material';
 import Button from '@mui/material/Button';
 import * as jobsAPI from '../../utilities/jobs-api';
 import { useNavigate } from 'react-router-dom';
@@ -29,31 +40,50 @@ export default function MyDashboardPage({ user }) {
   }
 
   return (
-    <Container
-      disableGutters
+    <Paper
       sx={{
-        maxHeight: 'calc(100vh - 64px)',
-        paddingTop: '64px',
+        margin: '15px',
       }}
     >
-      MyDashboardPage <br /> <br />
-      {jobs.map((job, idx) => (
-        <div key={idx}>
-          Name: {job.name} <br />
-          Description: {job.description} <br />
-          JobUserid: {job.userId._id} <br />
-          UserId: {user._id} <br />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}
-            onClick={() => handleEndJob(job._id)}
-          >
-            End Job
-          </Button>
-        </div>
-      ))}
-    </Container>
+      <Toolbar
+        disableGutters
+        sx={{
+          backgroundColor: 'rgb(241,247,254)',
+        }}
+      >
+        <Grid container>
+          <Grid item xs>
+            <Typography variant="h5">MyDashboardPage</Typography>
+          </Grid>
+        </Grid>
+      </Toolbar>
+
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableBody>
+            {jobs.map((job) => (
+              <TableRow key={job.name}>
+                <TableCell component="th" scope="row">
+                  <Button href={`/jobs/${job._id}`}>
+                    <Typography variant="h6">{job.name}</Typography>
+                  </Button>
+                </TableCell>
+                <TableCell align="right">
+                  <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2 }}
+                    onClick={() => handleEndJob(job._id)}
+                  >
+                    End Job
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Paper>
   );
 }
